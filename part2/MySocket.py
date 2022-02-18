30baab27eccd15c32c43876492d4ade063dd4e94
Mahdi-Rahmani <mah.rah79@yahoo.com>
2022-02-18 11:49:01 +0330

    send_message function added to MySocket

---
+++ part2/MySocket.py
@@ -0,0 +1,13 @@
+from socket import *
+import binascii
+
+def send_message(serverPort, serverAddress, message):
+    # now we should create a socket
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+    clientSocket.sendto(binascii.unhexlify(message), (serverAddress, serverPort))
+    response, serverAddress = clientSocket.recvfrom(2048)
+    response = binascii.hexlify(response).decode("utf-8")
+    clientSocket.close()
+    return response
+
+
