d1bfdee1cb09ab82e96a367f1f9e98139c2bf4b6
Mahdi-Rahmani <mah.rah79@yahoo.com>
2022-02-18 11:53:30 +0330

    some comments added

--- part2/MySocket.py
+++ part2/MySocket.py
@@ -1,6 +1,11 @@
 from socket import *
 import binascii
 
+""" This function is used after creating a DNS query.
+first we should create a socket. after that we send the query
+with this socket. then we recieve the server response and return it."""
+
+
 def send_message(serverPort, serverAddress, message):
     # now we should create a socket
     clientSocket = socket(AF_INET, SOCK_DGRAM)
@@ -10,12 +15,13 @@ def send_message(serverPort, serverAddress, message):
     clientSocket.close()
     return response
 
-serverAddress = '127.0.0.1'
+
+# if we want to use our server we should change the serverName in to 127.0.0.1 and comment the line 28
+# serverAddress = '127.0.0.1'
+serverAddress = '1.1.1.1'
 serverPort = 52
 message = '463f010000010000000000000377777706676f6f676c6503636f6d0000010001'
 
 response = send_message(serverPort,serverAddress,message)
 print('The response is:\n', response)
-
-
 
