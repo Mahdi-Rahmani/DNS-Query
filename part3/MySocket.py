92e5c31a11850d8beef87875f8552873301736e4
Mahdi-Rahmani <mah.rah79@yahoo.com>
2022-02-19 17:54:07 +0330

    MySocket file is created and list of DNS types added

---
+++ part3/MySocket.py
@@ -0,0 +1,4 @@
+# we can find the list of types from searching(specially wikipedia)
+types = ["ERROR", "A", "NS", "MD", "MF", "CNAME", "SOA", "MB", "MG", "MR", "NULL", "WKS", "PTR", "HINFO",
+         "MINFO", "MX", "TXT", 'RP', 'AFSDB', 'X25', 'ISDN', 'RT', 'NSAP', 'NSAP-PTR', 'SIG', 'KEY', 'PX',
+         'GPOS', 'AAAA','LOC', 'NXT', 'EID', 'NB', 'NBSTAT','ATMA']
