81b23ef7aa290ae628071d8ab2a1fa23b8e46646
Mahdi-Rahmani <mah.rah79@yahoo.com>
2022-02-19 17:46:21 +0330

    message_builder function added to DNSMessage class

--- part3/DNSMessage.py
+++ part3/DNSMessage.py
@@ -34,3 +34,22 @@ class DNSMessage:
             '{:04x}'.format(self.get_type(type)),
             '{:04x}'.format(QCLASS),
         ]
+    def message_builder(self):
+        # We hold the final message in message variable
+        # add the header in message
+        message = ''.join(self.header)
+        # add the address in message after some processes
+        address_parts = self.question[0].split(".")
+        for part in address_parts:
+            address_len = "{:02x}".format(len(part))
+            address_part = binascii.hexlify(part.encode())
+            message += address_len
+            message += address_part.decode()
+        # Terminating bits for QNAME
+        message += '00'
+
+        # add the other parts of question part
+        message += self.question[1]
+        message += self.question[2]
+        message = message.replace(" ", "").replace("\n", "")
+        return message
