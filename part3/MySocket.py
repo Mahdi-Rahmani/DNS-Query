94d929662eb64eac6cdecff08403c03b4ada774b
Mahdi-Rahmani <mah.rah79@yahoo.com>
2022-02-19 17:55:19 +0330

    user_interface function added

--- part3/MySocket.py
+++ part3/MySocket.py
@@ -2,3 +2,16 @@
 types = ["ERROR", "A", "NS", "MD", "MF", "CNAME", "SOA", "MB", "MG", "MR", "NULL", "WKS", "PTR", "HINFO",
          "MINFO", "MX", "TXT", 'RP', 'AFSDB', 'X25', 'ISDN', 'RT', 'NSAP', 'NSAP-PTR', 'SIG', 'KEY', 'PX',
          'GPOS', 'AAAA','LOC', 'NXT', 'EID', 'NB', 'NBSTAT','ATMA']
+
+def user_interface():
+    # we should get the address and type from user
+    name_address = input('Please enter the name address:\n')
+    while True:
+        DNSType = input('please enter the type of query:\n')
+        if DNSType.isdigit():
+            DNSType = int(DNSType)
+            if DNSType < len(types):
+                break
+        if DNSType in types:
+            break
+    return name_address, DNSType
